db7106e752b023c7bebe96f6cb11022ace26568f
poketopa <96972872+poketopa@users.noreply.github.com>
2026-05-07 17:19:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.80 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,25 +1,25 @@
 import java.util.*;
 class Solution {
-    public int solution(int[][] maps) {
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] visited = new int[maps.length][maps[0].length];
+    public int solution(int[][] map) {
+        int[] target = new int[]{map.length-1, map[0].length-1};
+        int[][] visited = new int[map.length][map[0].length];
         int[] y = {1, -1, 0, 0};
         int[] x = {0, 0, 1, -1};
+        Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 0, 1});
         visited[0][0] = 1;
-        
+            
         while(!queue.isEmpty()){
             int[] temp = queue.poll();
-            if(temp[0] == maps.length-1 && temp[1] == maps[0].length - 1){
+            if(target[0] == temp[0] && target[1] == temp[1]){
                 return temp[2];
             }
-            
             for(int i=0;i<4;i++){
                 int newY = temp[0] + y[i];
                 int newX = temp[1] + x[i];
-                if(0 <= newY && newY < maps.length &&
-                  0 <= newX && newX < maps[0].length &&
-                  maps[newY][newX] == 1 && 
+                if(0 <= newY && newY < map.length &&
+                  0 <= newX && newX < map[0].length &&
+                  map[newY][newX] == 1 &&
                   visited[newY][newX] == 0){
                     queue.add(new int[]{newY, newX, temp[2] + 1});
                     visited[newY][newX] = 1;
