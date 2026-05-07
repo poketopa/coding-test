93cff5e13528aaddc964be260a2391536ef9200e
poketopa <96972872+poketopa@users.noreply.github.com>
2026-05-07 14:22:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.83 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] maps) {
-        int[][] move = new int[maps.length][maps[0].length];
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0,0});
-        move[0][0]=1;
-
-        int distance = 0;
-        int nowX = 0;
-        int nowY = 0;
-
-        int[] x = {1,-1,0,0};
-        int[] y = {0, 0,1,-1};
-
-        while(queue.size()!=0){
-            int[] now = queue.poll();
-            maps[now[0]][now[1]]=0;
-
+        int[][] visited = new int[maps.length][maps[0].length];
+        int[] y = {1, -1, 0, 0};
+        int[] x = {0, 0, 1, -1};
+        queue.add(new int[]{0, 0, 1});
+        visited[0][0] = 1;
+        
+        while(!queue.isEmpty()){
+            int[] temp = queue.poll();
+            if(temp[0] == maps.length-1 && temp[1] == maps[0].length - 1){
+                return temp[2];
+            }
+            
             for(int i=0;i<4;i++){
-                int dx = now[0]+x[i];
-                int dy = now[1]+y[i];
-                if(dx>=0&&dx<maps.length&&dy>=0&&dy<maps[0].length&&maps[dx][dy]==1&&visited[dx][dy] == false){
-                    queue.offer(new int[]{dx,dy});
-                    move[dx][dy] = move[now[0]][now[1]]+1;
-                    visited[dx][dy] = true;
-                    if(dx==maps.length-1&&dy==maps[0].length-1){
-                        return move[dx][dy];
-                    }
+                int newY = temp[0] + y[i];
+                int newX = temp[1] + x[i];
+                if(0 <= newY && newY < maps.length &&
+                  0 <= newX && newX < maps[0].length &&
+                  maps[newY][newX] == 1 && 
+                  visited[newY][newX] == 0){
+                    queue.add(new int[]{newY, newX, temp[2] + 1});
+                    visited[newY][newX] = 1;
                 }
             }
-
         }
         return -1;
     }
