e707a275adda856e5b2289c4cab1660e69ea2fd5
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-02 18:39:19 +0900

    [level 2] Title: 가장 큰 수, Time: 1597.55 ms, Memory: 417 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util .*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] num = new String[numbers.length];
-        for(int i=0;i<numbers.length;i++) num[i] = String.valueOf(numbers[i]);
-        Arrays.sort(num, (a,b)->{
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(strNumbers, (a, b) -> {
             return (b+a).compareTo(a+b);
         });
         String answer = "";
-        for(String i : num) answer += i;
-        if(answer.charAt(0) == '0') return "0";
+        for (String i : strNumbers) {
+            answer += i;
+        }
+        if(strNumbers[0].equals("0")) return "0";
         return answer;
     }
 }
+// 올바른 정렬 34 3 30
+// 34 30 3
