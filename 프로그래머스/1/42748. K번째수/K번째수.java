b1a267c2b28cfd55bdc2dd341b58e5f9e96206cb
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-04 10:48:11 +0900

    [level 1] Title: K번째수, Time: 1.77 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,12 +3,12 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for(int i=0;i<commands.length;i++){
-            List<Integer> list = new ArrayList<>();
-            for(int j=commands[i][0]-1;j<commands[i][1];j++){
-                list.add(array[j]);
+            int[] temp = new int[commands[i][1] - commands[i][0] + 1];
+            for(int j=0;j<temp.length;j++){
+                temp[j] = array[commands[i][0] + j - 1];
             }
-            Collections.sort(list);
-            answer[i] = list.get(commands[i][2]-1);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
         }
         return answer;
     }
