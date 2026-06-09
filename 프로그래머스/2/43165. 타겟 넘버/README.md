7111e1fd2230bd5abc1355679cf065daa72a3057
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-09 10:19:27 +0900

    [level 2] Title: 타겟 넘버, Time: 3.45 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 85.8 MB, 시간: 7.19 ms
+메모리: 79.7 MB, 시간: 3.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 19:24:47
+2026년 06월 09일 10:19:21
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,40 +1,18 @@
-import java.util.*;
-import java.math.*;
-
 class Solution {
-    static int answer = 0;
-    static int sum = 0;
+    int count = 0;
     public int solution(int[] numbers, int target) {
-        // 합 만들기
-        for(int i : numbers) sum += i;
-        int[] visited = new int[numbers.length];
-
-        for(int i=0;i<numbers.length;i++){
-            dfs(numbers, target, visited, i);
-        }
-        return answer;
+        int now = 0;
+        
+        dfs(numbers, target, now, 0);
+        return count;
     }
-
-    private void dfs(int[] numbers, int target, int[] visited, int nowIndex){
-        // 1. visited 확인, 갱신
-        if(visited[nowIndex] == 1) return;
-        visited[nowIndex] = 1;
-        sum -= numbers[nowIndex] *  2;
-
-        // 2. 조건 확인
-        if(sum <= target){
-            if(sum == target){
-                answer++;
-            }
+    
+    public void dfs(int[] numbers, int target, int now, int index){
+        if(index == numbers.length) {
+            if(target == now) count++;
+            return;
         }
-        else{
-            // 3. 재귀
-            for(int i= nowIndex+1;i<numbers.length;i++){
-                dfs(numbers, target, visited, i);
-            }
-        }
-
-        sum += numbers[nowIndex] * 2;
-        visited[nowIndex] = 0;
+        dfs(numbers, target, now + numbers[index], index + 1);
+        dfs(numbers, target, now - numbers[index], index + 1);   
     }
 }
