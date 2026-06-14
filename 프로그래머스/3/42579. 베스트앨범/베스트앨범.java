c98c70246f68feaa77dc9d0278912dada3b3dc3d
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-14 18:53:55 +0900

    [level 3] Title: 베스트앨범, Time: 3.10 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,50 +1,39 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        // 1, 해시맵 (문자열, 정수) 만들기 2, 해시맵(문자열, 정수리스트(인덱스))) 만들기
-        HashMap<String, Integer> playSum = new HashMap<>();
-        HashMap<String, List<Integer>> indexMap = new HashMap<>();
-
+        Map<String, List<int[]>> map = new HashMap<>();
+        Map<String, Integer> sumMap = new HashMap<>();
+        Set<String> set = new HashSet<>();
         for(int i=0;i<genres.length;i++){
-            // 값이 없으면
-            if(!playSum.containsKey(genres[i])){
-                playSum.put(genres[i], 0);
-                indexMap.put(genres[i], new ArrayList<>());
+            if(!set.contains(genres[i])) { // 이미 존재하면 리스트에 더함
+                map.put(genres[i], new ArrayList<>());
+                set.add(genres[i]);
             }
-            playSum.put(genres[i], playSum.get(genres[i]) + plays[i]);
-            indexMap.get(genres[i]).add(i);
+            map.get(genres[i]).add(new int[]{i, plays[i]});
+            sumMap.put(genres[i], sumMap.getOrDefault(genres[i], 0) + plays[i]);
         }
-
-        // 장르 이름 배열
-        String[] music = playSum.keySet().toArray(new String[0]);
-        // 총 재생수로 정렬
-        Arrays.sort(music, (a, b)->{
-            if(playSum.get(a)<playSum.get(b)) return 1;
-            else if(playSum.get(a)>playSum.get(b)) return -1;
-            else return 0;
-        });
-        for(int i=0;i<music.length;i++){
-            Collections.sort(indexMap.get(music[i]), (a, b)->{
-                if(plays[a]<plays[b]) return 1;
-                else if(plays[a]>plays[b]) return -1;
-                else return 0;
+        List<String> list = new ArrayList<>();
+        for(String i : set){
+            list.add(i);
+            Collections.sort(map.get(i), (a, b) -> {
+                if(a[1] != b[1]){
+                    return b[1] - a[1];
+                }
+                return a[0] - b[0];
             });
         }
+        Collections.sort(list, (a, b) -> sumMap.get(b) - sumMap.get(a));
         List<Integer> answerList = new ArrayList<>();
-
-        for(int i=0;i<music.length;i++){
-            // 1개일 때
-            if(indexMap.get(music[i]).size() == 1){
-                answerList.add(indexMap.get(music[i]).get(0));
-            }
-            else{
-                answerList.add(indexMap.get(music[i]).get(0));
-                answerList.add(indexMap.get(music[i]).get(1));
+        for(int i=0;i<list.size();i++){
+            if(map.get(list.get(i)).size() == 1){
+                answerList.add(map.get(list.get(i)).get(0)[0]);
+            } else {
+                answerList.add(map.get(list.get(i)).get(0)[0]);
+                answerList.add(map.get(list.get(i)).get(1)[0]);
+                //Map<String, List<int[]>> map = new HashMap<>();
             }
         }
         int[] answer = new int[answerList.size()];
-
         for(int i=0;i<answer.length;i++) answer[i] = answerList.get(i);
         return answer;
     }
