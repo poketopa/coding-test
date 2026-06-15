c834fad1bbb2907370810cbfc5edad6df9a8132d
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-15 11:28:15 +0900

    [level 3] Title: 징검다리 건너기, Time: 90.12 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,19 +1,41 @@
 import java.util.*;
 class Solution {
     public int solution(int[] stones, int k) {
-        // 길이 n에서 최대값이 가장 작은 곳
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for(int i=0;i<k;i++){
-            pq.add(new int[]{i, stones[i]});
+        int max = -1;
+        for(int i : stones) max = Math.max(max, i);
+        
+        return search(1, max, stones, k);
+    }
+    
+    public int search(int start, int end, int[] stones, int k){
+        if(start >= end){
+            return start;
         }
-        int min = pq.peek()[1];
-        for(int i=k;i<stones.length;i++) {
-            pq.add(new int[]{i ,stones[i]});
-            while(pq.peek()[0] + k - 1 < i) {
-                 pq.poll();
+        int mid = (start + end) / 2;
+        
+        int[] after = new int[stones.length];
+        for(int i=0;i<stones.length;i++) {
+            after[i] = stones[i] - mid;
+        }
+        int count = count(after, k);
+        if(count >= k) {
+            return search(start, mid, stones, k);
+        } else {
+            return search(mid + 1, end, stones, k);
+        }
+    }
+    
+    public int count(int[] stones, int k){
+        int maxLength = -1;
+        int tempCount = 0;
+        for(int i=0;i<stones.length;i++){
+            if(stones[i] <= 0){
+                tempCount++;
+            } else {
+                tempCount = 0;
             }
-            min = Math.min(min, pq.peek()[1]);
+            maxLength = Math.max(maxLength, tempCount);
         }
-        return min;
+        return maxLength;
     }
 }
