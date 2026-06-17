dda6211e74c974b59108e6ea748155bbb24d0e3d
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-17 11:10:24 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.50 ms
+메모리: 73.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 28일 14:56:14
+2026년 06월 17일 11:10:18
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,53 +1,27 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        // 흠..
-        // 0 2 1 2 1
-        int[] student = new int[n];
-        // lost -> 1
-        for(int i : lost){
-            student[i-1] = 1;
-        }
-        for(int i=0;i<reserve.length;i++){
-            // 여벌이 도난당했을 경우
-            if(student[reserve[i]-1] == 1){
-                student[reserve[i]-1] = 0;
-                reserve[i] = -1;
-            }
-        }
-        for(int i : reserve){
-            if(i==-1) continue;
-            //맨 왼쪽 확인
-            if(i == 1){
-                if(student[1]==1){
-                    student[1] = 0;
-                    continue;
-                }
-            }
-            else if(i==n){
-                if(student[n-2]==1){
-                    student[n-2] = 0;
-                    continue;
-                }
-            }
-            else{
-                if(student[i-2]==1){
-                    student[i-2] = 0;
-                    continue;
-                }
-                else if(student[i] == 1){
-                    student[i] = 0;
-                    continue;
-                }
-            }
-        }
         int answer = 0;
-        for(int i : student){
-            if(i==0) answer++;
+        int[] arr = new int[n + 1];
+        for(int i : lost){
+            arr[i]--;
         }
+        for(int i : reserve) {
+            arr[i]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(arr[i] == -1){
+                if(arr[i-1] == 1){
+                    arr[i-1] = 0;
+                    arr[i] = 0;
+                } else if(i != n && arr[i+1] == 1){
+                    arr[i+1] = 0;
+                    arr[i] = 0;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+          if(arr[i] == 0 || arr[i] == 1) answer++;
+        } 
         return answer;
     }
 }
