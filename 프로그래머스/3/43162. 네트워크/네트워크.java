d3c8a9bbf7d9db536dd2e6f63ac37f081d83081f
poketopa <96972872+poketopa@users.noreply.github.com>
2026-06-21 10:16:25 +0900

    [level 3] Title: 네트워크, Time: 2.65 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,42 +1,25 @@
-import java.util.*;
-
-// 2차원 배열에 넣기
-// 확인하면서 한번 봤으면 visited만들어서 1 넣기
 class Solution {
-    static int answer = 0;
-
     public int solution(int n, int[][] computers) {
-        int[] visited = new int[computers.length];
-
-
-        // 흠...
-        // 1. i 로 0, 1, 2 들어간다
-        // 2. i가 1일 때 : dfs i가 2이면 dfs(1) 들어감
-        //               2에서 1일 때 dfs(1)들어감 (들어가면 안됨 visited == 0)
-        //               2에서 3일 때 dfs(3)들어감
-        for(int i=0;i<computers.length;i++){
-            if(visited[i]==0){
-                answer++;
-            }
-            dfs(computers, i, visited);
-
+        int[] visited = new int[n];
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(visited[i] == 1) continue;
+            count++;
+            visited[i] = 1;
+            dfs(i, computers, visited);   
         }
-        return answer;
-
+        return count;
     }
-
-    private static void dfs(int[][] computers, int index, int[] visited){
-        // 이미 연결됐던 네트워크면 돌아감
-        if(visited[index] == 1){
-            return;
-        }
-        // 아니면 끝가지 파고듦
+    
+    public void dfs(int now, int[][] computers, int[] visited){
         for(int i=0;i<computers.length;i++){
-
-            if(computers[index][i]==1&&visited[i]==0){
-                visited[index] = 1;
-                dfs(computers, i, visited);
+            if(visited[i] == 1 || now == i || computers[now][i] == 0) {
+                continue;
             }
+            visited[i] = 1;
+            System.out.println("dfs 탐색: " + i);
+            dfs(i, computers, visited);
+            
         }
     }
 }
