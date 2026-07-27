8f333ec788838aa9292cf44e44ab7b30c4ef88e3
poketopa <96972872+poketopa@users.noreply.github.com>
2026-07-27 13:41:36 +0900

    [level 3] Title: 섬 연결하기, Time: 1.19 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,48 +1,41 @@
 import java.util.*;
-
 class Solution {
-    static int min = Integer.MAX_VALUE;
+    static int[] parent;
     public int solution(int n, int[][] costs) {
-        List<int[]>[] map = new List[n];
-        for(int i=0;i<map.length;i++){
-            map[i] = new ArrayList<>();
+        int sum = 0;
+        int edgeCount = 0;
+        
+        parent = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] = i;
         }
+        Arrays.sort(costs, (a, b) -> {
+            return a[2] - b[2];
+        });
+        
         for(int i=0;i<costs.length;i++){
-            int start = costs[i][0];
-            int end = costs[i][1];
+            int a = costs[i][0];
+            int b = costs[i][1];
             int cost = costs[i][2];
-            map[start].add(new int[]{end, cost});
-            map[end].add(new int[]{start, cost});
-        }
-
-        int[] visited = new int[n];
-        int count = 0;
-        int now = 0;
-
-        List<Integer> history = new ArrayList<>();
-
-        // 0에서 시작
-        visited[0] = 1;
-        history.add(0);
-        while(history.size()!=n){
-            int min = Integer.MAX_VALUE;
-            int minIndex = 0;
-            // 현재 찾은 것들에서
-            for(int i=0;i<history.size();i++){
-                // 가장 짧은 것들 확인
-                for(int j=0;j<map[history.get(i)].size();j++){
-                    if(visited[map[history.get(i)].get(j)[0]] == 1) continue;
-                    if(min>map[history.get(i)].get(j)[1]) {
-                        min = map[history.get(i)].get(j)[1];
-                        minIndex = map[history.get(i)].get(j)[0];
-                    }
-                }
+            
+            if(find(a) != find(b)){
+                sum += cost;
+                edgeCount++;
+                union(a, b);
             }
-            history.add(minIndex);
-            count += min;
-            visited[minIndex] = 1;
+            if(edgeCount == n - 1) return sum;
         }
-
-        return count;
+        return 0;
+    }
+    
+    private int find(int x) {
+        if(parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+    
+    private void union(int a, int b){
+        if(find(a) != find(b)) parent[find(b)] = find(a);
     }
 }
